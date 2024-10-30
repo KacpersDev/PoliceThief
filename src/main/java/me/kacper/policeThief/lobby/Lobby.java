6893c13abefe93829bcb006f78e4d77853d6eac8
KacperM <vkacperpl@gmail.com>
2024-10-30 21:11:32 +0100

    .

--- src/main/java/me/kacper/policeThief/lobby/Lobby.java
+++ src/main/java/me/kacper/policeThief/lobby/Lobby.java
@@ -3,6 +3,7 @@ package me.kacper.policeThief.lobby;
 import lombok.Getter;
 import lombok.Setter;
 import me.kacper.policeThief.ModeType;
+import me.kacper.policeThief.PoliceThief;
 import org.bukkit.Location;
 
 @Getter
@@ -13,9 +14,23 @@ public class Lobby {
     private Location spawnPoint;
     private boolean canMove;
 
+    private int currentPlayers, maxPlayers, minPlayers, countdownTimer;
+
     public Lobby(ModeType modeType, Location spawnPoint, boolean canMove) {
         this.modeType = modeType;
         this.spawnPoint = spawnPoint;
         this.canMove = canMove;
+
+        this.assignValues();
+        this.currentPlayers = 0;
+    }
+
+    private void assignValues() {
+        this.maxPlayers = PoliceThief.getInstance().getConfiguration().getConfiguration()
+                .getInt("lobbies." + modeType.getConfigType() + ".max-players");
+        this.minPlayers = PoliceThief.getInstance().getConfiguration().getConfiguration()
+                .getInt("lobbies." + modeType.getConfigType() + ".min-players");
+        this.countdownTimer = PoliceThief.getInstance().getConfiguration().getConfiguration()
+                .getInt("lobbies." + modeType.getConfigType() + ".countdown-timer");
     }
 }
---
+++ src/main/java/me/kacper/policeThief/lobby/listener/LobbyListener.java
@@ -0,0 +1,13 @@
+package me.kacper.policeThief.lobby.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class LobbyListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+
+    }
+}
